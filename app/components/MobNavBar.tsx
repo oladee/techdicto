ee7d5d6f11d344dc9b6ca991edcfba0ecc8cbd03
oladee <deemajor230600@gmail.com>
2024-11-18 16:21:30 +0100

    redo on menu pop up

--- app/components/MobNavBar.tsx
+++ app/components/MobNavBar.tsx
@@ -8,17 +8,22 @@ interface props {
     setMob : React.Dispatch<React.SetStateAction<boolean>>
 }
 const MobNavBar = ({mob, setMob}:props) => {
+    const idea = mob
     useEffect(()=>{
-        if(mob){
+        if(idea){
             document.body.classList.add('bodyStiff')
         }else{
-            document.body.classList.remove('bodyStiff')
+            
         }
-    })
+    },[idea])
+    const handleCloseMenu = ()=>{
+        setMob(false)
+        document.body.classList.remove('bodyStiff')
+    }
   return (
     <div className='text-black bg-[rgba(255,255,255,0.2)] w-full fixed h-screen z-50 top-0 right-0 md:hidden  ' data-aos='fade-right'>
         <div className='bg-white-100 h-screen w-[75%] ml-auto p-5 rounded-l-2xl'>
-            <Image src={closeIcon} alt='Techdicto close menu' className='ml-auto w-14 cursor-pointer' onClick={()=> {setMob(false)}}/>
+            <Image src={closeIcon} alt='Techdicto close menu' className='ml-auto w-14 cursor-pointer' onClick={handleCloseMenu}/>
             <div className=" items-center gap-6 flex flex-col z-50 text-black">
                 <Link href='#features' data-aos='fade-right'>
                 Features
